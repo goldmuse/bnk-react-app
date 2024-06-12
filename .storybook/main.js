e058c1b3afee4cf28f33d4b82a65a574bb6c5cfb
김호정 <goldmuse@naver.com>
2024-06-12 17:24:24 +0900

    storybook 설치 커밋

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+/** @type { import('@storybook/nextjs').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",ㅞ
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+};
+export default config;
